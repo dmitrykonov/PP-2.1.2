545b7f3b2fa0aeaec6b915c5c7b94e4f19403594
Dmitry Konovalov <dmitrykonov@mail.ru>
2022-06-25 02:51:44 +0300

    Сделал задание

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,22 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat catBean2 =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean compareBeans = bean == bean2;
+        boolean compareCats = catBean == catBean2;
+
+        System.out.println("comparison result for HelloWorld beans is " + compareBeans);
+        System.out.println("comparison result for Cat beans is " + compareCats);
+
         System.out.println(bean.getMessage());
     }
 }
